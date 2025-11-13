5175c9cfc24d09ecdbe4e5bbdfcd4580b9e15944
Isira Hansaja <153202522+iSiRaH@users.noreply.github.com>
2025-11-13 22:24:22 +0530

    Login ui

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,25 @@
+import Logo from "../assets/logo.png";
 import "../styles/login.css";
 
 function Login() {
   return (
     <>
-      <div className="test">
-        <h1 className="pb-4 text-blue-500">Login form</h1>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button>Login</button>
+      <div className="login-container">
+        <h1 className="login-title">Crime Link Analyzer</h1>
+        <p className="login-description">Crime Investigation Data Intelligent System</p>
+        <img src={Logo} alt="Logo" className="login-logo" />
+        <input type="text" placeholder="Username" className="input-username" />
+        <input type="password" placeholder="Password" className="input-password" />
+        <div>
+          <input type="checkbox" id="rememberMe" className="chkbox-rememberMe" />
+          <label className="rememberMe-txt"> Remember Me</label>
+        </div>
+        <button className="login-btn">Login</button>
+        <a href="#" className="forget-password">Forgot Password?</a>
+        <p className="bottom-text">
+          Access to this System is restricted to authorized personnel of Sri
+          Lanka Crime Division only.
+        </p>
       </div>
     </>
   );
