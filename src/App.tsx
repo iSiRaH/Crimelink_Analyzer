78c9b3fd76a0d39fdd9e7dbc23115ef9401c42cf
Arosha Udaraka <aroshaudaraka99@gmail.com>
2025-11-14 23:58:23 +0530

    add topbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Sidebar from "./components/Sidebar";
 import Login from "./pages/Login";
+import Topbar from "./components/Topbar";
 
 function App() {
   return (
     <>
       <div>
         {/* <Login /> */}
+        <Topbar />
         <Sidebar />
       </div>
     </>
