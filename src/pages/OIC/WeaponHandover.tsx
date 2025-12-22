7dca3de6d70777eb70dede69b263f1b290d294fe
dasunithimasha <dasunithimasha25@gmail.com>
2025-12-22 16:18:01 +0530

    connect Return weapon window

--- src/pages/OIC/WeaponHandover.tsx
+++ src/pages/OIC/WeaponHandover.tsx
@@ -1,10 +1,18 @@
 import { useState } from "react";
 import { Search } from "lucide-react";
 import IssueWeaponModal from "./IssueWeaponModal";
-
+import ReturnWeaponModal from "./ReturnWeaponModal";  
 export default function WeaponHandover() {
   const weapons = [
     { type: "AK 47", serial: "G-1359", status: "Available", assignedTo: "--", dueBack: "--" },
+    { type: "Minimi", serial: "T-4579", status: "Issued", assignedTo: "J.B.Millawithanachchi", dueBack: "27/11/2025" },
+    { type: "Space 12", serial: "G-0851", status: "Available", assignedTo: "--", dueBack: "--" },
+    { type: "Glock 17", serial: "K-6438", status: "Issued", assignedTo: "A.B.C.Bandara", dueBack: "03/12/2025" },
+    { type: "AK 47", serial: "G-1359", status: "Available", assignedTo: "--", dueBack: "--" },
+    { type: "Minimi", serial: "T-4579", status: "Issued", assignedTo: "J.B.Millawithanachchi", dueBack: "27/11/2025" },
+    { type: "Space 12", serial: "G-0851", status: "Available", assignedTo: "--", dueBack: "--" },
+    { type: "Glock 17", serial: "K-6438", status: "Issued", assignedTo: "A.B.C.Bandara", dueBack: "03/12/2025" },
+     { type: "AK 47", serial: "G-1359", status: "Available", assignedTo: "--", dueBack: "--" },
     { type: "Minimi", serial: "T-4579", status: "Issued", assignedTo: "J.B.Millawithanachchi", dueBack: "27/11/2025" },
     { type: "Space 12", serial: "G-0851", status: "Available", assignedTo: "--", dueBack: "--" },
     { type: "Glock 17", serial: "K-6438", status: "Issued", assignedTo: "A.B.C.Bandara", dueBack: "03/12/2025" },
@@ -20,6 +28,8 @@ export default function WeaponHandover() {
   /* ================= MODAL STATES ================= */
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedWeapon, setSelectedWeapon] = useState<any>(null);
+
+  const [isReturnOpen,setIsReturnOpen]=useState(false);
 
   const filteredWeapons = weapons.filter((w) => {
     const matchesSearch =
@@ -39,6 +49,10 @@ export default function WeaponHandover() {
     setIsModalOpen(true);
   };
 
+  const onReturnClick=()=>{
+    setIsReturnOpen(true);
+  }
+
   return (
     
     <div className="relative flex min-h-screen bg-[#3b4a5f] text-white">
@@ -47,6 +61,8 @@ export default function WeaponHandover() {
       <main
         className={`flex-1 p-3 transition-all duration-300 ${
           isModalOpen ? "opacity-20 pointer-events-none" : "opacity-100"
+        }  ${
+          isReturnOpen ? "opacity-20 pointer-events-none" : "opacity-100"
         }`}
       >
         {/* HEADER */}
@@ -102,7 +118,7 @@ export default function WeaponHandover() {
           </div>
 
           {/* TABLE */}
-          <table className="w-full text-md">
+          <table className="w-full text-md  ">
             <thead className="text-gray-400 border-b-2 bg-[#3b4a5f] border-gray-700">
               <tr>
                 <th className="py-1 text-left pl-5">Weapon Type</th>
@@ -113,10 +129,10 @@ export default function WeaponHandover() {
                 <th className="text-left">Action</th>
               </tr>
             </thead>
-
+            
             <tbody>
               {filteredWeapons.map((w, index) => (
-                <tr key={index} className="border-b border-gray-800">
+                <tr key={index} className="border-b border-gray-800 max-h-[50vh] overflow-y-auto no-scrollbar ">
                   <td className="py-2 pl-5">{w.type}</td>
                   <td>{w.serial}</td>
                   <td className={`font-semibold ${w.status === "Available" ? "text-green-500" : "text-red-500"}`}>
@@ -133,7 +149,9 @@ export default function WeaponHandover() {
                         Issue
                       </button>
                     ) : (
-                      <button className="border border-red-500 text-red-500 px-5 rounded-full text-md hover:bg-red-500 hover:text-white w-24">
+                      <button
+                      onClick={() => onReturnClick()}
+                      className="border border-red-500 text-red-500 px-5 rounded-full text-md hover:bg-red-500 hover:text-white w-24">
                         Return
                       </button>
                     )}
@@ -150,6 +168,13 @@ export default function WeaponHandover() {
         <IssueWeaponModal
           weapon={selectedWeapon}
           onClose={() => setIsModalOpen(false)}
+        />
+      )}
+
+         {isReturnOpen && (
+        <ReturnWeaponModal
+          weapon={selectedWeapon}
+          onClose={() => setIsReturnOpen(false)}
         />
       )}
     </div>
