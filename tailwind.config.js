5301d7570f6f899f245f03ae86c866318aef426a
Lal Pushpakumaran <161225569+LPK98@users.noreply.github.com>
2026-02-11 18:05:23 +0530

    Plate Registry UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-bg": "#0b0c1a",
+        "dark-panel": "#181d30",
+        "dark-border": "#2a3148",
+        "dark-border-light": "#3a4158",
+      },
+    },
   },
   plugins: [],
 };
